ea0acb12861e6c2b01702615b9d7fcf0afa99c4d
KomanBoni <145965405+KomanBoni@users.noreply.github.com>
2023-11-03 18:24:19 +0000

    Update jeux.js

--- src/compenant/jeux.js
+++ src/compenant/jeux.js
@@ -95,7 +95,7 @@ function Combat() {
 
         {character && (
           <div>
-            <img src={character.image} alt={character.name} id="ntm" />
+            <img src={character.image} alt={character.name} id="perso" />
             <h3 className="character-text">Votre personnage : {character.name}</h3>
             <p className="health-text-green">Health: {character.health}</p>
             {character.techniques.map((technique, index) => (
